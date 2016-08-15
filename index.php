52b0b6e9109585ea7c2f94df77fc117cf1fb8f0b
hontein <mrm1989@mail.ru>
2016-08-15 04:04:35 +0400

    Исправлены ссылки, внешний вид

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "list.php";
 $auth = new AuthClass();
 if (isset($_POST["login"]) && isset($_POST["password"])) { //If username and password were sent
     if ($r = $auth->auth($_POST["login"], $_POST["password"])) {
-        echo $text[$r];
+        echo "<script>window.onload = function(){document.getElementById('error_msg_login').innerHTML ='" . $text[$r] . "';}</script>";
     }
 }
 
@@ -16,37 +16,77 @@ if (isset($_GET["is_exit"])) { //If you press the exit button
         header("Location: ?is_exit=0"); //Redirect after
     }
 }
+
+
 ?>
 
 <?php if ($auth->isAuth()) { // If the user is logged in, welcome :
-    echo "Здравствуйте, " . $auth->getLogin();
-    echo "<br/><br/><a href=\"?is_exit=1\">Выйти</a>"; //Show the escape button
+    echo "<div align='right'>Здравствуйте, " . $auth->getLogin();
+    echo "<br/><br/><a href=\"panel.php?kartoteka=1\">Панель управления</a>"; //Show the escape button
+    echo "<br/><br/><a href=\"?is_exit=1\">Выйти</a></div>";
 } else { //If not authorized , showing the shape of a login and password
     ?>
-    <form method="post" action="">
-        Логин: <input type="text" name="login"
-                      value="<?php echo (isset($_POST["login"])) ? $_POST["login"] : null; // Fill in the field by default ?>"/><br/>
-        Пароль: <input type="password" name="password" value=""/><br/>
-        <input type="submit" value="Войти"/>
-    </form>
+    <div align="right"><h2>Авторизация на сайте:</h2>
+
+        <form action="" method="post">
+            <table>
+                <tr>
+                    <td>(test)Логин:</td>
+                    <td><input type="text" name="login"
+                               value="<?php echo (isset($_POST["login"])) ? $_POST["login"] : null; // Fill in the field by default ?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>(test)Пароль:</td>
+                    <td><input type="password" name="password" value=""/>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td align="right"><input type="submit" name="Войти"></td>
+                </tr>
+            </table>
+        </form>
+        <div id="error_msg_login" style="color: red">&nbsp;</div>
+    </div>
+
 <?php } ?>
-    <hr>
+<hr>
+<div align="center"><h2>Поиск книг на сайте:</h2>
     <form method="post" action="">
-        Наименование книги: <input type="text" name="name_book"
-                                   value="<?php echo (isset($_POST["name_book"])) ? $_POST["name_book"] : null; // Fill in the field by default ?>"/><br/>
-        Автор: <input type="text" name="author" value=""/><br/>
-        <input type="submit" value="Найти"/>
+        <table>
+            <tr>
+                <td>
+                    Наименование книги:
+                </td>
+                <td><input type="text" name="name_book"
+                           value="<?php echo (isset($_POST["name_book"])) ? $_POST["name_book"] : null; // Fill in the field by default ?>"/>
+                </td>
+            </tr>
+            <tr>
+                <td>Автор:</td>
+                <td><input type="text" name="author" value=""/></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td align="right"><input type="submit" value="Найти"/>
+            </tr>
+        </table>
     </form>
+</div>
+
 <?php
 if (isset($_POST["name_book"]) or isset($_POST["author"])) {
     $db = new MysqlClass(SERVER, USER, PASS, DBNAME);
-    $temp = "author INNER JOIN records ON author.id_author = records.id_author LEFT JOIN books ON records.id_book = books.id_book WHERE author.name LIKE '%" . $_POST["name_book"] .
-        "%' AND books.name LIKE '%" . $_POST["author"] . "%'";
-    $q = $db->select("author.name AS 'author', books.name AS 'name'", $temp);
+    $temp = "author INNER JOIN records ON author.id_author = records.id_author LEFT JOIN books ON records.id_book = books.id_book WHERE author.name LIKE '%" . $_POST["author"] .
+        "%' AND books.name LIKE '%" . $_POST["name_book"] . "%'";
+    if ($q = $db->select("author.name AS 'author', books.name AS 'name'", $temp)) {
 
 
-    ?>
-    <table border="1">
+        ?>
+        <table border="1">
         <tr>
             <td>Автор</td>
             <td>Название</td>
@@ -59,10 +99,14 @@ if (isset($_POST["name_book"]) or isset($_POST["author"])) {
                 <td><?= $value['author'] ?></td>
                 <td><?= $value['name'] ?></td>
             </tr>
-        <?php } ?>
 
-    </table>
+        <?php }
+        ?>  </table><?php
+    } else {
+        echo "<div>Книги не найдены </div>";
+    } ?>
     <?php
 
 
 }
+?>
